2b8cca47bfaadeafb93bc0963157c77cfec8d201
simbawu <code@simbawu.com>
2019-08-21 21:00:19 +0800

    chore: add delpoy docs page script

---
+++ config/deploy-docs.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack');
+const webpackConfig = require('./webpack/webpack.config');
+const simpleGit = require('simple-git');
+
+new Promise((resolve, reject) => {
+  webpack({ mode: 'development', ...webpackConfig }).run((err, stats) => {
+    if (err) {
+      reject(err);
+    } else {
+      console.log(stats.toString(webpackConfig.stats));
+      resolve();
+    }
+  });
+}).then(() => {
+  console.log('deploy docs start');
+  simpleGit()
+    .add('.')
+    .commit('feat: update docs pages')
+    .push(['origin', 'master'], () => console.log('deploy docs done'));
+});
