551c0d25ff1bce7a6b85b0e0af21e877b15b7525
simbawu <code@simbawu.com>
2019-08-29 15:19:11 +0800

    chore: add api demo

--- demo/index.js
+++ demo/index.js
@@ -1,5 +1,11 @@
 import Calendar from '../src/calendar.js';
 
 new Calendar({
-  el: document.querySelector('#demo')
+  el: document.querySelector('#demo'),
+  currentDate: '2019/08/28',
+  onDayClick
 });
+
+function onDayClick(date) {
+  console.log(date);
+}
